354e9aff1cb9a12b10ac83cb792ac837b2c74831
Laila Alaa <lailaalaa644@gmail.com>
2026-05-03 18:48:18 +0300

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
   return (
     <div>
       <h1>Character Counter</h1>
-      <input type="text" value={text} onChange={handleChange} placeholder="Type something..."/>
+      <input type="text" value={text} maxLength={30} onChange={(e) => setText(e.target.value)} placeholder="Type something..."/>
       <p>Characters: {text.length}/30</p>
       <p> {status} </p>
     </div>
