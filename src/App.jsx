84d9e31523f7b13daea3dc3ac1f6ecb8f1850729
Laila Alaa <lailaalaa644@gmail.com>
2026-05-03 18:04:24 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,34 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import './App.css'
 
 function App() {
- 
+  const list = [
+    "iPhone 15 Pro",
+    "Samsung Galaxy S24",
+    "MacBook Pro M3",
+    "AirPods Pro 2",
+    "Google Pixel 8",
+    "PlayStation 5",
+    "Nintendo Switch",
+    "Dell XPS 13",
+    "iPad Pro",
+    "Sony WH-1000XM5"
+  ]
 
-  return (
-    <>
-     
-
-    </>
+  const [data,setdata]=useState("");
+  const [update, setupdete]=useState([]);
+  useEffect(()=>{
+    const timer = setTimeout(()=>{
+      setupdete(list.filter((t)=>t.toLowerCase().includes(data.toLowerCase())))
+    },500)
+  },[data])
+  return(
+    <div>
+      <input placeholder='search' onChange={(e)=>{setdata(e.target.value)}}/>
+      {update.map((item,index)=>{return<li>{item}</li>})}
+    </div>
   )
 }
+
 
 export default App
