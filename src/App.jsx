61becaa2c45351b8c14b5af2fe45c1b361a1b87c
Laila Alaa <lailaalaa644@gmail.com>
2026-05-03 19:41:01 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,18 @@ import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const list = [
-    "iPhone 15 Pro",
-    "Samsung Galaxy S24",
-    "MacBook Pro M3",
-    "AirPods Pro 2",
-    "Google Pixel 8",
-    "PlayStation 5",
-    "Nintendo Switch",
-    "Dell XPS 13",
-    "iPad Pro",
-    "Sony WH-1000XM5"
-  ]
+const list=[ "iphon" ,"hawawy" , "samsong"]
+const [data,setdata]=useState("");
+const [update , setupdate]=useState([]);
+useEffect (()=>{
+  const timer = setTimeout(()=>{ setupdate(list.filter((t)=>t.includes(data)))
 
-  const [data,setdata]=useState("");
-  const [update, setupdete]=useState([]);
-  useEffect(()=>{
-    const timer = setTimeout(()=>{
-      setupdete(list.filter((t)=>t.toLowerCase().includes(data.toLowerCase())))
-    },500)
-  },[data])
-  return(
-    <div>
-      <input placeholder='search' onChange={(e)=>{setdata(e.target.value)}}/>
-      {update.map((item,index)=>{return<li>{item}</li>})}
-    </div>
-  )
+  },500 )
+},[data])
+return (<div>
+  <input placeholder='enter search' onChange={(e)=>{setdata(e.target.value)}}/>
+  {update.map((item,index)=>{return <li>{item}</li>})}
+</div>)
 }
 
 
