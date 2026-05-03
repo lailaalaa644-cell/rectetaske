c6084e4cbe964da70f18c185a769526612ce65fa
Laila Alaa <lailaalaa644@gmail.com>
2026-05-03 19:53:52 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,23 @@
 import React, { useState, useEffect } from "react";
 
 function App() {
-  const [text, setText] = useState("");
-  const [status, setStatus] = useState("");
-
-  useEffect(() => {
-    if (text.length < 10) {
-      setStatus("Too short");
-    } else if (text.length <= 20) {
-      setStatus("Good");
-    } else {
-      setStatus("Too long");
-    }
-  }, [text]);
-
-  return (
-    <div>
-      <h1>Character Counter</h1>
-      <input type="text" value={text} maxLength={30} onChange={(e) => setText(e.target.value)} placeholder="Type something..."/>
-      <p>Characters: {text.length}/30</p>
-      <p> {status} </p>
-    </div>
-  );
+ const [text , settext]=useState("");
+ const [state , setstate]=useState("");
+ useEffect(()=>{
+  if(text.length<10){
+    setstate("Too Short")
+  }else if(text.length<=20){
+    setstate("Good")
+  }else{
+    setstate("to long")
+  }
+ },[text]);
+ return(
+  <div>
+    <input placeholder="dmck" type="text" value={text} onChange={(e)=>{settext(e.target.value)}} maxLength={30}/>
+    <p>{text.length}/30</p>
+    <p>{state}</p>
+  </div>
+ )
 }
 export default App;
